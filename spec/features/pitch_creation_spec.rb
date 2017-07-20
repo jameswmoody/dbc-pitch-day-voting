571529fcdbdf8e3be9c9bef403b51f334d6f5be0
Desmond Naranjo <dhnaranjo@gmail.com>
2017-07-20 13:44:43 -0500

    Removed 2.3 syntax heredoc tilde

--- spec/features/pitch_creation_spec.rb
+++ spec/features/pitch_creation_spec.rb
@@ -7,12 +7,12 @@ feature "pitch creation" do
 
       pitch = {
         title: 'Grapefruit Delivery Wearable',
-        description: <<~END,
-        Catching unsuspecting sources completely off guard with their remarks, the populace of the city of Chicago, entirely unprompted, announced Monday, “We get the food and then we eat the food until all the food is gone.”
+        description: <<-END,
+Catching unsuspecting sources completely off guard with their remarks, the populace of the city of Chicago, entirely unprompted, announced Monday, “We get the food and then we eat the food until all the food is gone.”
 
-        Reporters—who had not made inquiries in regard to the topic and remained uncertain as to why it had been brought up at all—said that several moments of silence followed the abrupt declaration as all 10 million residents of the greater Chicago metropolitan area stood there, smiling broadly.
+Reporters—who had not made inquiries in regard to the topic and remained uncertain as to why it had been brought up at all—said that several moments of silence followed the abrupt declaration as all 10 million residents of the greater Chicago metropolitan area stood there, smiling broadly.
 
-        “We get the food first, and then we eat the food,” the populace of the third-largest American city said by way of clarification, reportedly interrupting confused sources as they attempted tentative responses. “Then when the food is gone, we go home and watch TV. Sometimes we eat the food during TV, too. Or sometimes after TV.”
+“We get the food first, and then we eat the food,” the populace of the third-largest American city said by way of clarification, reportedly interrupting confused sources as they attempted tentative responses. “Then when the food is gone, we go home and watch TV. Sometimes we eat the food during TV, too. Or sometimes after TV.”
         END
         pitchers: 'The Onion' 
       }
