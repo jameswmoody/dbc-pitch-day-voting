f5003206b67bc613af6a89380879e2636e741e74
James Moody & Josh Harvey <pair+jameswmoody.jmharvey0@devbootcamp.com>
2017-07-19 21:01:12 -0500

    Write test for routes and controllers, pass all test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe UsersController do
+  describe "GET #token" do
+    it "responds successfully with an HTTP 200 status code" do
+
+      get :create
+      expect(response).to have_http_status(302)
+    end
+  end
+end
