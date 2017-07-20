c81688c85c51c804f65702771d9ef237c71a05e2
James Moody & Josh Harvey <pair+jameswmoody.jmharvey0@devbootcamp.com>
2017-07-20 09:02:47 -0500

    fix typos and explore how things were working

--- spec/routing/user_route_spec.rb
+++ spec/routing/user_route_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "routing for users", :type => :routing do
-  it "routes /auth/:provider/callback to users#token" do
+  it "routes /auth/:provider/callback to users#create" do
     expect(:get => "/auth/github/callback").to route_to(
         :controller => "users",
         :action => "create"
