a34d114485235d0cf49881ba7aa770f7ac9908de
Rachel <rachel.kerner@gmail.com>
2017-07-20 11:12:14 -0500

    Write tests for splash page controller/route/view, pass all tests from red to green. Create splash page controller, model, view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'pitches#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#welcome'
+
+  get '/pitches', to: 'pitches#index'
+
   get '/users', to: 'users#index'
   get "/auth/github/callback", to: "users#create", as: :create
 end
