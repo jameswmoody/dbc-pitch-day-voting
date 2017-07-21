305081232f576887ef6663f8e156bb7632663264
Desmond Naranjo <dhnaranjo@gmail.com>
2017-07-20 20:14:26 -0500

    Can assign users at creation/login, reject non members

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,24 @@
 class User < ApplicationRecord
+  belongs_to :cohort
+
+
   def self.find_or_create_from_auth(auth)
-    user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
+    nickname = auth['info']['nickname']
+    valid_user = ValidUser.find_by(nickname: nickname)
 
-    user.nickname = auth['info']['nickname']
-    user.name = auth['info']['name']
-    user.email = auth['info']['email']
-    user.token = auth['credentials']['token']
+    if valid_user
+      user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
 
-    user.save
-    user
+      user.nickname = nickname
+      user.name = auth['info']['name']
+      user.email = auth['info']['email']
+      user.token = auth['credentials']['token']
+      user.cohort = valid_user.cohort
+
+      user.save
+      user
+    else
+      p "NONNA THAT SHIT"
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720231124) do
+ActiveRecord::Schema.define(version: 20170721005841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20170720231124) do
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "cohort_id"
+    t.index ["cohort_id"], name: "index_users_on_cohort_id"
   end
 
   create_table "valid_users", force: :cascade do |t|
@@ -47,5 +49,6 @@ ActiveRecord::Schema.define(version: 20170720231124) do
     t.index ["cohort_id"], name: "index_valid_users_on_cohort_id"
   end
 
+  add_foreign_key "users", "cohorts"
   add_foreign_key "valid_users", "cohorts"
 end
